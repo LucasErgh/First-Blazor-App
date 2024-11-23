1fba77de19631c5de0ede3447016e89468867d16
Lucas Ergh <lucasergh@gmail.com>
2024-11-23 02:40:14 -0500

    started project with a tic tac toe component

---
+++ Components/Pages/Counter.razor.cs
@@ -0,0 +1,10 @@
+namespace ToDo_Webapp.Components.Pages{
+
+    public partial class Counter{
+        private int currentCount = 0;
+
+        private void IncrementCount(){
+            currentCount++;
+        }
+    }
+}
